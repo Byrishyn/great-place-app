efae1655e68f4723358cd6fb2cd0893a7aad5b59
Leroux Antoine <antoine.d.m.leroux@gmail.com>
2021-09-06 14:49:43 +0900

    Save data to sqlite

--- store/place-actions.js
+++ store/place-actions.js
@@ -1,5 +1,6 @@
 import * as FileSystem from "expo-file-system"
 export const ADD_PLACE = "ADD_PLACE"
+import { insertPlace } from "../helpers/db"
 
 export const addPlace = (title, image) => {
     return async dispatch => {
@@ -7,15 +8,15 @@ export const addPlace = (title, image) => {
         const newPath = FileSystem.documentDirectory + fileName
 
         try {
-            await 
-            FileSystem.moveAsync({
+            await FileSystem.moveAsync({
                 from: image,
                 to: newPath
             })
+            const dbResult = await insertPlace(title, newPath, "Dummy address", 15.3, 16.3)
+            dispatch({ type: ADD_PLACE, placeData: { id: dbResult.insertId, title: title, image: newPath } })
         } catch (err) {
             console.log(err)
             throw err;
         }
-        dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } })
     }
 }
